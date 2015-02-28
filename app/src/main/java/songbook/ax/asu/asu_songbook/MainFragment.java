f75304a28b80bb7446368152bc096e2b9620fc42
jakelamotta <krjo7211@student.uu.se>
2015-02-28 11:55:50 +0100

    init commit

---
+++ app/src/main/java/songbook/ax/asu/asu_songbook/MainFragment.java
@@ -0,0 +1,67 @@
+package songbook.ax.asu.asu_songbook;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
+
+/**
+ * Created by EIS i7 Gamer on 2015-02-26.
+ */
+public class MainFragment extends Fragment {
+
+    private static final String LOG_TAG = MainFragment.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.v(LOG_TAG,"in onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.list_view_songs);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.v(LOG_TAG,"clicked");
+            }
+        });
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
---
+++ app/src/main/java/songbook/ax/asu/asu_songbook/data/SongSyncAdapter.java
@@ -0,0 +1,7 @@
+package songbook.ax.asu.asu_songbook.data;
+
+/**
+ * Created by EIS i7 Gamer on 2015-02-26.
+ */
+public class SongSyncAdapter{
+}
